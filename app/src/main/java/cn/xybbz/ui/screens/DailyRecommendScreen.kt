a8d784f9e23fcd889989f5e0fd70cda268303394
刘梦龙 <893043230@qq.com>
2025-11-05 17:54:43 +0800

    增加推荐列表页面

---
+++ app/src/main/java/cn/xybbz/ui/screens/DailyRecommendScreen.kt
@@ -0,0 +1,9 @@
+package cn.xybbz.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DailyRecommendScreen(modifier: Modifier = Modifier) {
+
+}
