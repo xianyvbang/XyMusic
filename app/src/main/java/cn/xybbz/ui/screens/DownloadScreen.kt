d3986bd6fe0373993c589c9f27788b78ffdd48c6
刘梦龙 <893043230@qq.com>
2026-01-13 17:33:03 +0800

    修复下载失败后无法点击继续下载的问题

--- app/src/main/java/cn/xybbz/ui/screens/DownloadScreen.kt
+++ app/src/main/java/cn/xybbz/ui/screens/DownloadScreen.kt
@@ -1,3 +1,21 @@
+/*
+ *   XyMusic
+ *   Copyright (C) 2023 xianyvbang
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 package cn.xybbz.ui.screens
 
 import androidx.compose.animation.AnimatedContent
@@ -185,7 +203,7 @@ fun DownloadItemTrailingContent(
                     } else {
                         if (task.status == DownloadStatus.DOWNLOADING || task.status == DownloadStatus.QUEUED) {
                             onPause()
-                        } else if (task.status == DownloadStatus.PAUSED) {
+                        } else if (task.status == DownloadStatus.PAUSED || task.status == DownloadStatus.FAILED) {
                             onResume()
                         }
                     }
