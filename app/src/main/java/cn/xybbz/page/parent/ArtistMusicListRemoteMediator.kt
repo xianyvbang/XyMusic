7782bbb8c54039992dd6e11e00ebfb964c48fa9e
刘梦龙 <893043230@qq.com>
2026-01-14 17:21:08 +0800

    修改ArtistMusicListRemoteMediator继承DefaultRemoteMediator

--- app/src/main/java/cn/xybbz/page/parent/ArtistMusicListRemoteMediator.kt
+++ app/src/main/java/cn/xybbz/page/parent/ArtistMusicListRemoteMediator.kt
@@ -1,18 +1,30 @@
+/*
+ *   XyMusic
+ *   Copyright (C) 2023 xianyvbang
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 package cn.xybbz.page.parent
 
 import androidx.paging.ExperimentalPagingApi
-import androidx.paging.LoadType
-import androidx.paging.PagingState
-import androidx.paging.RemoteMediator
-import androidx.room.withTransaction
 import cn.xybbz.api.client.IDataSourceParentServer
-import cn.xybbz.common.constants.Constants
+import cn.xybbz.api.client.data.XyResponse
 import cn.xybbz.common.constants.RemoteIdConstants
 import cn.xybbz.localdata.config.DatabaseClient
 import cn.xybbz.localdata.data.music.XyMusic
-import cn.xybbz.localdata.data.remote.RemoteCurrent
 import cn.xybbz.localdata.enums.MusicDataTypeEnum
-import java.util.concurrent.TimeUnit
 
 @OptIn(ExperimentalPagingApi::class)
 class ArtistMusicListRemoteMediator(
@@ -20,88 +32,43 @@ class ArtistMusicListRemoteMediator(
     private val datasourceServer: IDataSourceParentServer,
     private val db: DatabaseClient,
     private val connectionId: Long
-) : RemoteMediator<Int, XyMusic>() {
+) : DefaultRemoteMediator<XyMusic, XyMusic>(
+    db,
+    RemoteIdConstants.ARTIST + RemoteIdConstants.MUSIC + artistId + connectionId,
+    connectionId
+) {
 
-    private val remoteKeyDao = db.remoteCurrentDao
-    private val remoteId = RemoteIdConstants.ARTIST + RemoteIdConstants.MUSIC + artistId
-
-    override suspend fun load(
-        loadType: LoadType,
-        state: PagingState<Int, XyMusic>,
-    ): MediatorResult {
-        return try {
-
-            val loadKey: Int = when (loadType) {
-                LoadType.REFRESH -> 0
-                LoadType.PREPEND -> return MediatorResult.Success(
-                    endOfPaginationReached = true
-                )
-
-                LoadType.APPEND -> {
-                    val remoteKey = db.withTransaction {
-                        remoteKeyDao.remoteKeyById(remoteId)
-                    }
-                    if (remoteKey == null || (remoteKey.nextKey * state.config.pageSize) >= remoteKey.total
-                    ) {
-                        return MediatorResult.Success(
-                            endOfPaginationReached = true
-                        )
-                    }
-                    remoteKey.nextKey.plus(1)
-                }
-            }
-
-            val response =
-                datasourceServer.selectMusicListByArtistServer(
-                    pageSize = state.config.pageSize,
-                    startIndex = loadKey * state.config.pageSize,
-                    artistId = artistId
-                )
-            db.withTransaction {
-                if (loadType == LoadType.REFRESH) {
-                    remoteKeyDao.deleteById(remoteId)
-                    db.musicDao.removeByType(MusicDataTypeEnum.ARTIST, artistId)
-                }
-
-                remoteKeyDao.insertOrReplace(
-                    RemoteCurrent(
-                        id = remoteId,
-                        nextKey = loadKey,
-                        total = response.totalRecordCount,
-                        connectionId = connectionId
-                    )
-                )
-
-                response.items?.let {
-                    datasourceServer.saveBatchMusic(
-                        it,
-                        MusicDataTypeEnum.ARTIST,
-                        artistId = artistId
-                    )
-                }
-
-            }
-
-            MediatorResult.Success(
-                endOfPaginationReached = response.items.isNullOrEmpty()
-                        || (loadKey * state.config.pageSize) >= response.totalRecordCount
-            )
-
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            MediatorResult.Error(e)
-        }
+    /**
+     * 获得远程服务对象列表
+     * @param [loadKey] 页码 从0开始
+     * @param [pageSize] 页面大小
+     */
+    override suspend fun getRemoteServerObjectList(
+        loadKey: Int,
+        pageSize: Int
+    ): XyResponse<XyMusic> {
+        return datasourceServer.selectMusicListByArtistServer(
+            pageSize = pageSize,
+            startIndex = loadKey * pageSize,
+            artistId = artistId
+        )
     }
 
-    override suspend fun initialize(): InitializeAction {
-        val cacheTimeout = TimeUnit.MILLISECONDS.convert(Constants.PAGE_TIME_FAILURE, TimeUnit.MINUTES)
-        return if (System.currentTimeMillis() - (remoteKeyDao.remoteKeyById(remoteId)?.createTime
-                ?: 0) <= cacheTimeout
-        ) {
-            InitializeAction.SKIP_INITIAL_REFRESH
-        } else {
-            InitializeAction.LAUNCH_INITIAL_REFRESH
-        }
+    /**
+     * 删除本地数据库对象列表
+     */
+    override suspend fun removeLocalObjectList() {
+        db.musicDao.removeByType(MusicDataTypeEnum.ARTIST, artistId)
+    }
+
+    /**
+     * 存储对象列表到本地数据库
+     */
+    override suspend fun saveBatchLocalObjectList(items: List<XyMusic>) {
+        datasourceServer.saveBatchMusic(
+            items,
+            MusicDataTypeEnum.ARTIST,
+            artistId = artistId
+        )
     }
 }
