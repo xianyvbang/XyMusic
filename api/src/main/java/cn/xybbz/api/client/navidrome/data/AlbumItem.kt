7ced24b378f58f25693dd8817ffa96a725ba6eb7
刘梦龙 <893043230@qq.com>
2026-01-16 10:10:57 +0800

    修复Navidrome返回对象部分属性的空值序列化报错

--- api/src/main/java/cn/xybbz/api/client/navidrome/data/AlbumItem.kt
+++ api/src/main/java/cn/xybbz/api/client/navidrome/data/AlbumItem.kt
@@ -1,3 +1,21 @@
+/*
+ *   XyMusic
+ *   Copyright (C) 2023 xianyvbang
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 package cn.xybbz.api.client.navidrome.data
 
 import com.squareup.moshi.JsonClass
@@ -19,11 +37,11 @@ data class AlbumItem(
     /**
      * 播放次数
      */
-    val playCount: Long,
+    val playCount: Long? = null,
     /**
      * 播放时间
      */
-    val playDate: LocalDateTime,
+    val playDate: LocalDateTime? = null,
     /**
      * 评价星级
      */
@@ -134,4 +152,4 @@ data class AlbumItem(
 
     val mbzReleaseGroupId: String? = null,
 
-)
+    )
--- api/src/main/java/cn/xybbz/api/client/navidrome/data/ArtistItem.kt
+++ api/src/main/java/cn/xybbz/api/client/navidrome/data/ArtistItem.kt
@@ -1,3 +1,21 @@
+/*
+ *   XyMusic
+ *   Copyright (C) 2023 xianyvbang
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
 package cn.xybbz.api.client.navidrome.data
 
 import com.squareup.moshi.JsonClass
@@ -54,7 +72,7 @@ data class ArtistItem(
     /**
      * 外部信息更新时间
      */
-    val externalInfoUpdatedAt: LocalDateTime,
+    val externalInfoUpdatedAt: LocalDateTime? = null,
     /**
      * 是否数据缺失
      */
@@ -74,7 +92,7 @@ data class ArtistItem(
     /**
      * 艺术家排序名称
      */
-    val orderArtistName: String,
+    val orderArtistName: String ? = null,
     /**
      * mbz艺术家id
      */
