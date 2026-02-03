f1724d8c2870df3953e05d7213db0890e3e722cd
刘梦龙 <893043230@qq.com>
2026-02-03 11:05:38 +0800

    修改app的build.gradle.kts里的kotlin{}的位置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,11 +100,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget = JvmTarget.JVM_21
-        }
-    }
+
     buildFeatures {
         compose = true
         buildConfig = true
@@ -125,6 +121,12 @@ android {
         }
     }
 
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
+    }
 }
 composeCompiler {
 
