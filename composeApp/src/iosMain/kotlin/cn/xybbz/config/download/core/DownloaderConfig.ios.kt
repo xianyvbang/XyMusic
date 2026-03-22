9146a149379b70aa520966630d8b15d021bb293d
xybbz <liu893043230@gmail.com>
2026-03-22 23:45:36 +0800

    删除下载功能

--- composeApp/src/iosMain/kotlin/cn/xybbz/config/download/core/DownloaderConfig.ios.kt
+++
@@ -1,9 +0,0 @@
-package cn.xybbz.config.download.core
-
-import cn.xybbz.di.ContextWrapper
-import cn.xybbz.localdata.config.documentDirectory
-
-actual fun getDefaultDownloadDirectory(contextWrapper: ContextWrapper): String {
-    return documentDirectory() + "/${DOWNLOAD_FILE_NAME}"
-}
-
--- composeApp/src/jvmMain/kotlin/cn/xybbz/config/download/core/DownloaderConfig.jvm.kt
+++
@@ -1,8 +0,0 @@
-package cn.xybbz.config.download.core
-
-import cn.xybbz.di.ContextWrapper
-import java.io.File
-
-actual fun getDefaultDownloadDirectory(contextWrapper: ContextWrapper): String {
-    return  File(System.getProperty("java.io.tmpdir"), DOWNLOAD_FILE_NAME).path
-}
