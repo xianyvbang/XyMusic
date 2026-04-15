e513db2672b03077001364b7dece8f9e36d30203
刘梦龙 <893043230@qq.com>
2026-04-15 17:52:42 +0800

    增加侧边栏展开功能

--- composeApp/src/jvmMain/kotlin/cn/xybbz/ui/screens/MainScreenScaffold.jvm.kt
+++ composeApp/src/jvmMain/kotlin/cn/xybbz/ui/screens/MainScreenScaffold.jvm.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.WideNavigationRail
 import androidx.compose.material3.WideNavigationRailItem
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import cn.xybbz.router.NavigationState
 import cn.xybbz.router.Navigator
@@ -14,8 +15,15 @@ import cn.xybbz.router.PlatformNavigationConfig
 import cn.xybbz.router.jvmTopRouterDataList
 import cn.xybbz.ui.xy.XyRow
 import cn.xybbz.ui.xy.XyText
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
+import androidx.compose.material3.WideNavigationRailValue
+import androidx.compose.material3.rememberWideNavigationRailState
+import xymusic_kmp.composeapp.generated.resources.Res
+import xymusic_kmp.composeapp.generated.resources.close
+import xymusic_kmp.composeapp.generated.resources.close_24px
+import xymusic_kmp.composeapp.generated.resources.menu_open_24px
 
 @Composable
 actual fun MainScreenScaffold(
@@ -26,6 +34,10 @@ actual fun MainScreenScaffold(
     snackbarHost: @Composable () -> Unit,
     content: @Composable (PaddingValues) -> Unit
 ) {
+    val railState = rememberWideNavigationRailState()
+    val coroutineScope = rememberCoroutineScope()
+    val railExpanded = railState.currentValue == WideNavigationRailValue.Expanded
+
     Scaffold(
         modifier = modifier,
 //        snackbarHost = snackbarHost
@@ -33,6 +45,38 @@ actual fun MainScreenScaffold(
         XyRow(paddingValues = PaddingValues()) {
             WideNavigationRail(
                 modifier = Modifier.padding(paddingValues),
+                state = railState,
+                header = {
+                    WideNavigationRailItem(
+                        selected = false,
+                        onClick = {
+                            coroutineScope.launch {
+                                if (railExpanded) {
+                                    railState.collapse()
+                                } else {
+                                    railState.expand()
+                                }
+                            }
+                        },
+                        icon = {
+                            Icon(
+                                painter = painterResource(
+                                    if (railExpanded) Res.drawable.close_24px
+                                    else Res.drawable.menu_open_24px
+                                ),
+                                contentDescription = if (railExpanded) {
+                                    stringResource(Res.string.close)
+                                } else {
+                                    "展开侧边栏"
+                                }
+                            )
+                        },
+                        label = {
+                            XyText(text = if (railExpanded) "收起" else "展开")
+                        },
+                        railExpanded = railExpanded,
+                    )
+                },
             ) {
                 jvmTopRouterDataList.forEach { item ->
                     WideNavigationRailItem(
@@ -45,7 +89,7 @@ actual fun MainScreenScaffold(
                             )
                         },
                         label = { XyText(text = stringResource(item.title)) },
-                        railExpanded = false,
+                        railExpanded = railExpanded,
                     )
                 }
             }
